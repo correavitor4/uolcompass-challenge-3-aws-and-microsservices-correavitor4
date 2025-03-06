3035305ee3da550ccee969250cafe9e844832dbf
Correa <correavitor4@hotmail.com>
2025-03-05 21:50:01 -0300

    config: creating event service

---
+++ event-service/src/test/java/compassoulspring2024pb/challenge1/eventservice/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package compassoulspring2024pb.challenge1.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
