670c72aed710c79760352156249c3bb4b07d9122
Correa <correavitor4@hotmail.com>
2025-03-05 23:03:13 -0300

    [36-create-event-model-and-repository] test: testing model and repository

--- event-service/src/main/java/compassoulspring2024pb/challenge1/eventservice/model/Event.java
+++ event-service/src/main/java/compassoulspring2024pb/challenge1/eventservice/model/Event.java
@@ -1,7 +1,7 @@
 package compassoulspring2024pb.challenge1.eventservice.model;
 
 import compassoulspring2024pb.challenge1.eventservice.model.enums.StatesEnum;
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,33 +9,50 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Document(collection = "events")
-@Getter
+@Data
 public class Event {
     @Id
     private UUID id = UUID.randomUUID();
 
     private String name;
-    private Instant createdAt;
+    private Instant createdAt = Instant.now();
     private String cep;
     private String address;
     private String city;
     private String district;
 
+    private Instant deletedAt;
+
     private StatesEnum state;
 
     public Event(String name,
-                 Instant createdAt,
                  String cep,
                  String address,
                  String city,
                  String district,
                  StatesEnum state) {
         this.name = name;
-        this.createdAt = createdAt;
         this.cep = cep;
         this.address = address;
         this.city = city;
         this.district = district;
         this.state = state;
+    }
+
+    public void setAsDeleted(){
+        this.deletedAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+        return id.equals(event.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
     }
 }
