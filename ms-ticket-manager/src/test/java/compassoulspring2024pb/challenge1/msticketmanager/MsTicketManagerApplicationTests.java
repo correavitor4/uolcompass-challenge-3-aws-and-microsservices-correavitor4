065372c776bb9ba25d65cf8eec3cbac29c689fdd
Correa <correavitor4@hotmail.com>
2025-03-10 16:08:47 -0300

    [11-create-ms-ticket-manager] config: creating ms-ticket-manager project

---
+++ ms-ticket-manager/src/test/java/compassoulspring2024pb/challenge1/msticketmanager/MsTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package compassoulspring2024pb.challenge1.msticketmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
