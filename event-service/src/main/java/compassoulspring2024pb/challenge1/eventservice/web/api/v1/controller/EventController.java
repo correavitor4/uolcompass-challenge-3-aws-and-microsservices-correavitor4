e96e6b74a88f001e45cfe5709bcbfe6cb49da81a
Correa <correavitor4@hotmail.com>
2025-03-13 15:33:38 -0300

    Reapply "[preparing-ticket-manager-for-deploy] docs: documenting EventController"
    This reverts commit fe04c5b20852f6440f094a2dfe9c94504cda87ab.

--- event-service/src/main/java/compassoulspring2024pb/challenge1/eventservice/web/api/v1/controller/EventController.java
+++ event-service/src/main/java/compassoulspring2024pb/challenge1/eventservice/web/api/v1/controller/EventController.java
@@ -16,7 +16,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
